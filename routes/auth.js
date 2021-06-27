f5a0ef21cdccc4398ada77636a7064320f1ad3e5
mridul1024 <mridul1024@gmail.com>
2021-06-27 20:46:51 +0530

    Planning authentication feature - auth (routes module)

---
+++ routes/auth.js
@@ -0,0 +1,13 @@
+/**
+ * * This module is reponsible for registration and authentication only
+ * ! Don't write business logic here for registration and login functions
+ *
+ * ? So, I want to store the user data in the database. How should I do that?
+ * ? I will create a route -> controller -> service -> repository pathway
+ * Todo: Create AuthController
+ * Todo: Write registration and login route -> Redirect it to AuthController
+ * Todo: Send a result back to the requesting user
+ */
+
+// Todo: Create registration route
+// Todo: Create Login route
