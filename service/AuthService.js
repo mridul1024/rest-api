7ae5e1d4c09929a6eed0fc1a1d13ace1b45886d8
mridul1024 <mridul1024@gmail.com>
2021-06-30 15:04:49 +0530

    AuthService - Registration - Password Hashing

--- service/AuthService.js
+++ service/AuthService.js
@@ -21,30 +21,48 @@ const AuthRepository = require("../repository/AuthRepository");
 const authRepository = new AuthRepository();
 const User = require("../model/UserModel");
 const generateApiKey = require("generate-api-key");
+const bcrypt = require("bcrypt");
+const { restart } = require("nodemon");
+
+//* Bcrypt hash rounds
+const saltRounds = 10;
 
 class AuthenticationService {
   //* Registration service
   async registrationService(req, res) {
     let result;
     let statusCode = "201";
+    let newUser;
 
-    //* User object
-    let newUser = new User({
-      username: req.body.username,
-      password: req.body.password,
-      apiKey: generateApiKey(), //generate api key and enter here,
-      fullName: req.body.fullName,
-      email: req.body.email,
+    //* Hashing password
+    bcrypt.hash(req.body.password, saltRounds, async (err, hash) => {
+      if (err) {
+        console.log(`Hash error: ${err}`);
+        result = err;
+        statusCode = "500";
+        return res.status(statusCode).json(result);
+      } else {
+        //* User object
+        newUser = new User({
+          username: req.body.username,
+          password: hash,
+          apiKey: generateApiKey(), //* generate api key and enter here,
+          fullName: req.body.fullName,
+          email: req.body.email,
+        });
+
+        try {
+          result = await authRepository.registration(newUser);
+          result == null ? (statusCode = "500") : (statusCode = "201");
+          res.status(statusCode).json(result);
+        } catch (error) {
+          console.log(`AuthService.js error: ${result}`);
+          result = error;
+          statusCode = "500";
+          res.status(statusCode).json(result);
+        }
+      }
     });
-
-    try {
-      result = await authRepository.registration(newUser);
-      result == null ? (statusCode = "500") : (statusCode = "201");
-    } catch (error) {
-      console.log(`AuthService.js error: ${result}`);
-    }
-
-    return res.status(statusCode).json(result);
   }
 
   //* Login service
